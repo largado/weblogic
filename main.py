4424a1ff8812a9f4dce8583b77deeb129d8387a2
largado <Kv2940bm@>
2022-08-31 01:56:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+p_server = "AdminServer1x"
+x=1
+
+
+for x in range(1):
+    if p_server == "AdminServer" or p_server == "AdminServer1":
+        continue
+
+    else:
+        print ("_stopServer(p_server)")
