f01ab3b0f13862710c7d2045fd64d46e3461ed18
largado <Kv2940bm@>
2022-08-31 19:03:06 -0300

    com quebra de linha

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-p_server = "AdminServer1x"
+p_server = "AdminServerxxxxx"
 x=1
 
 
 for x in range(1):
-    if p_server == "AdminServer" or p_server == "AdminServer1":
+    if p_server == "AdminServer" or p_server == "AdminServer1" or p_server == "AdminServer2" or p_server == "AdminServer3" \
+or p_server == "AdminServer3":
         continue
 
     else:
